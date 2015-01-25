a37d1abd6c3a4529f8b4012ae8235dc56f40bfb4
Patrick Silva <patrickalex@gmail.com>
2015-01-24 22:17:27 -0200

    Fix Samsung data filename

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 
 
 ## Stops execution if there is no Samsung data inside working directory.
-if(!file.exists(".\\UCI HAR Datasets")){
+if(!file.exists(".\\UCI HAR Dataset")){
         stop(paste("run_analysis.R can be run as long as the Samsung data is in
 your working directory:",getwd(),sep=" " ))
 }
